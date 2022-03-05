6fcf52c99f09a76193a8334f80b0c80c0b0c6f83
Tom <tommusique974@gmail.com>
2022-03-05 19:26:03 +0100

    feat(*) : initial commit

---
+++ pages/404.jsx
@@ -0,0 +1,9 @@
+const Error404 = () => {
+    return (
+        <>
+            <h1>Pas non trouvé</h1>
+        </>
+    )
+}
+
+export default Error404
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <h1>Hello From Next</h1>
+        </>
+    );
+};
+
+export default Home;
